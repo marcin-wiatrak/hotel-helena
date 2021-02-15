50291ea827049506044283c62dd09c1ef6d71052
marcin-wiatrak <marcin.wiatrak.95@gmail.com>
2021-02-15 20:49:29 +0100

    Added footer - unfinished yet

--- src/App.js
+++ src/App.js
@@ -2,38 +2,42 @@ import React from 'react';
 import Header from './Components/Header';
 import Hotel from './Views/Hotel/Hotel';
 import Pokoje from './Views/Pokoje/Pokoje'
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
-
+import { Route, Switch} from 'react-router-dom'
+import Footer from './Components/Footer/Footer'
 const App = () => (
-  <Router>
+
+  
+  <>
     <Header />
     <Switch>
       <Route path="/" exact>
         <Hotel />
       </Route>
-      <Route path="/pokoje" exact>
+      <Route path="/pokoje" >
         <Pokoje />
       </Route>
-      <Route path="/" exact>
+      <Route path="/mini-spa" >
         {/* <Main /> */}
       </Route>
-      <Route path="/" exact>
+      <Route path="/" >
         {/* <Main /> */}
       </Route>
-      <Route path="/" exact>
+      <Route path="/" >
         {/* <Main /> */}
       </Route>
-      <Route path="/" exact>
+      <Route path="/" >
         {/* <Main /> */}
       </Route>
-      <Route path="/" exact>
+      <Route path="/" >
         {/* <Main /> */}
       </Route>
-      <Route path="/" exact>
+      <Route path="/" >
         {/* <Main /> */}
       </Route>
     </Switch>
-  </Router>
+    <Footer/>
+
+  </>
 )
 
 export default App;
