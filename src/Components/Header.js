71747b224eabf45944f380137750193120554f52
marcin-wiatrak <marcin.wiatrak.95@gmail.com>
2021-02-17 21:12:40 +0100

    RWD for navbar and Header section

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,35 +1,37 @@
 import React, { useState } from 'react';
 import Nav from './Navbar/Nav';
 import styles from './Header.module.scss';
-import logo_main from '../assets/images/logo_main.svg';
-import slogan from '../assets/images/slogan.svg';
+import { ReactComponent as LogoMain } from '../assets/images/logo_main.svg';
+import { ReactComponent as Slogan } from '../assets/images/slogan.svg';
 
 const Header = () => {
-  const [isVisible, setIsVisible] = useState(false);
+    const [isVisible, setIsVisible] = useState(false);
 
-  const toggleNav = () => {
-    setIsVisible((prev) => !prev);
-  };
+    const toggleNav = () => {
+        setIsVisible((prev) => !prev);
+    };
 
-  const style = styles.hamburger;
-  const style2 = styles.active;
+    const style = styles.hamburger;
+    const style2 = styles.active;
 
-  const hamburgerClasses = isVisible ? style + ' ' + style2 : styles.hamburger;
+    const hamburgerClasses = isVisible
+        ? style + ' ' + style2
+        : styles.hamburger;
 
-  return (
-    <header className={styles.wrapper}>
-      <div className={styles.navbar}>
-        <img src={logo_main} />
-        <button className={hamburgerClasses} onClick={toggleNav}>
-          <div className={styles.bar1}></div>
-          <div className={styles.bar2}></div>
-          <div className={styles.bar3}></div>
-        </button>
-      </div>
-      <Nav isVisible={isVisible} />
-      <img src={slogan} className={styles.slogan}></img>
-    </header>
-  );
+    return (
+        <header className={styles.wrapper}>
+            <div className={styles.navbar}>
+                <LogoMain className={styles.logo} />
+                <button className={hamburgerClasses} onClick={toggleNav}>
+                    <div className={styles.bar1}></div>
+                    <div className={styles.bar2}></div>
+                    <div className={styles.bar3}></div>
+                </button>
+                <Nav isVisible={isVisible} />
+            </div>
+            <Slogan className={styles.slogan} />
+        </header>
+    );
 };
 
 export default Header;
