490a65fea8ed6126d25c607d8f2fc2e8e1314eec
marcin-wiatrak <marcin.wiatrak.95@gmail.com>
2021-02-17 21:22:54 +0100

    Prettier & ESLint repair for build errors

--- src/index.js
+++ src/index.js
@@ -5,10 +5,10 @@ import './styles.scss';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Router>
+            <App />
+        </Router>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
